7282bb53ccf16ffcfce1a472480a3eba2e874c33
Muhammad Shahid <shahid.jamal0343@gmail.com>
2018-01-28 10:48:25 +0500

    added contact page

--- js/app.js
+++ js/app.js
@@ -17,5 +17,16 @@ function slider(){
             $($(".slide-navigation li")[i]).find("span").toggleClass("active");
         }
     }
-    
+}
+
+function initMap(){
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map-section'), {
+        zoom: 8,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
 }
